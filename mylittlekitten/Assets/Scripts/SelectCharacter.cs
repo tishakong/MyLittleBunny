f05f20c734fb07efbae214a5e8808966716cd0e9
tishakong <leehn45@naver.com>
2024-01-20 18:44:17 +0900

    selectcharacter
    selectcharacter

--- mylittlekitten/Assets/Scripts/SelectCharacter.cs
+++ mylittlekitten/Assets/Scripts/SelectCharacter.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class SelectCharacter : MonoBehaviour
 {
     public Character character;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    public SelectCharacter[] chars;
 
     void Start()
     {
-        
+        animator= GetComponent<Animator>();
+        spriteRenderer= GetComponent<SpriteRenderer>();
+        OnDeSelect();
     }
 
     private void OnMouseUpAsButton()
     {
         DataManager.Instance.currentCharacter = character;
+        OnSelect();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] != this)
+            {
+                chars[i].OnDeSelect();
+            }
+        }
     }
-}
+
+    void OnDeSelect()
+    {
+        animator.SetBool("Walking", false);
+        spriteRenderer.transform.localScale = new Vector3(2f, 2f, 1f);
+    }
+
+    void OnSelect()
+    {
+        animator.SetBool("Walking", true);
+        spriteRenderer.transform.localScale = new Vector3(2.5f,2.5f,1f);
+     
+    }
+
+} 
