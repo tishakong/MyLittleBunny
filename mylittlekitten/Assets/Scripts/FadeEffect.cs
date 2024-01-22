9735eb1846fb8b1b41e42e006151af904a52112b
Jiwon, Cha <81519167+wona825@users.noreply.github.com>
2024-01-23 02:14:15 +0900

    feat/impl fade-inout

---
+++ mylittlekitten/Assets/Scripts/FadeEffect.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeEffect : MonoBehaviour
+{
+    private CanvasGroup cg;
+    public float fadeTime = 1f;
+    float accumTime = 0f;
+    private Coroutine fadeCor;
+    
+    private void Awake() 
+    {
+        cg = gameObject.GetComponent<CanvasGroup>();
+        StartFadeIn();
+    }
+
+    public void StartFadeIn()
+    {
+        if (fadeCor != null) 
+        {
+            StopAllCoroutines();
+            fadeCor = null;
+        }
+        fadeCor = StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        yield return new WaitForSeconds(0.2f);
+        accumTime = 0f;
+        while (accumTime < fadeTime)
+        {
+            cg.alpha = Mathf.Lerp(0f, 1f, accumTime / fadeTime);
+            yield return 0;
+            accumTime += Time.deltaTime;
+        }
+        cg.alpha = 1f;
+
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(1.0f);
+        accumTime = 0f;
+        while (accumTime < fadeTime)
+        {
+            cg.alpha = Mathf.Lerp(1f, 0f, accumTime / fadeTime);
+            yield return 0;
+            accumTime += Time.deltaTime;
+        }
+        cg.alpha = 0f;
+    }
+}
+
