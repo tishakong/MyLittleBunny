f65a27d3da95bdb64e4e55b056a6c90b8720c65d
tishakong <leehn45@naver.com>
2024-01-22 17:04:02 +0900

    레이캐스트 발사 수정 (길이 조절 필요함)
    레이캐스트 발사 수정 (길이 조절 필요함)

--- mylittlekitten/Assets/Scripts/CharacterMove.cs
+++ mylittlekitten/Assets/Scripts/CharacterMove.cs
@@ -8,15 +8,22 @@ public class CharacterMove : MonoBehaviour
     //걷기 위한 변수
     public float speed;
     private Vector3 vector;
+    public float runSpeed;
+    private float applyRunSpeed;
 
 
     //걷는 애니메이션 변수
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
+    //오브젝트 감지 변수
+    private CapsuleCollider2D capsuleCollider;
+    public LayerMask layerMask;
+    public float raycastLength = 0.000001f;
 
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -28,30 +35,65 @@ public class CharacterMove : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetAxisRaw("Horizontal")!=0 || Input.GetAxisRaw("Vertical") != 0)
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (horizontalInput != 0 || verticalInput != 0)
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+            }
+            else
+                applyRunSpeed = 0;
+
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x !=0)
+                vector.y = 0;
+
+
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
-            animator.SetBool("Walking", true);
 
-           
-            if (vector.x != 0)
+            RaycastHit2D hit;
+
+            Vector2 inputDirection = new Vector2(horizontalInput, verticalInput).normalized;
+            Vector2 start = capsuleCollider.bounds.center;
+            Vector2 end = start + inputDirection* raycastLength ;
+
+            capsuleCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            Debug.DrawRay(start, inputDirection * raycastLength, Color.red);
+            capsuleCollider.enabled = true;
+
+            if (vector.x < 0)
             {
-                transform.Translate(vector.x * speed, 0, 0);
-                
-                if (vector.x < 0)
-                {
-                    spriteRenderer.flipX = true;
-                }
-                else
-                {
-                    spriteRenderer.flipX= false;
-                }
+                spriteRenderer.flipX = true;
             }
-            else if (vector.y != 0)
+            else
             {
-                transform.Translate(0, vector.y * speed, 0);
+                spriteRenderer.flipX = false;
+            }
+
+            if (hit.transform != null)
+            {
+                print("무언가 맞음");
+                animator.SetBool("Walking", false);
+            }
+            else
+            {
+                animator.SetBool("Walking", true);
+
+
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
             }
         }
         else
@@ -59,12 +101,5 @@ public class CharacterMove : MonoBehaviour
             animator.SetBool("Walking", false);
         }
     }
-
-    private void FixedUpdate()
-    {
-        
-    }
-
-
 }
  
