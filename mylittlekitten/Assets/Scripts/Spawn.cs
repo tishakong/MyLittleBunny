7fe5a9ef2e7e22fdfe2ab7d0ef4047744b907866
tishakong <leehn45@naver.com>
2024-01-23 01:54:13 +0900

    Inside to Map 이동 구현 완료
    Inside to Map 이동 구현 완료

--- mylittlekitten/Assets/Scripts/Spawn.cs
+++ mylittlekitten/Assets/Scripts/Spawn.cs
@@ -12,13 +12,41 @@ public class Spawn : MonoBehaviour
     void Start()
     {
         player = Instantiate(charPrefabs[(int)DataManager.Instance.currentCharacter]);
+        string previousScene = PlayerPrefs.GetString("PreviousScene");
+        print(previousScene);
         player.transform.position = new Vector3 (transform.position.x, transform.position.y,0);
 
         if (SceneManager.GetActiveScene().name == "MainMap")
         {
+            if (string.IsNullOrEmpty(previousScene))
+            {
+                player.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            }
+            else if(previousScene == "BakeryShop")
+            {
+                player.transform.position = new Vector3(-5.5f, 1, 0);
+            }
+            else if (previousScene == "cafe_black_ver")
+            {
+                player.transform.position = new Vector3(30, -10, 0);
+            }
+            else if (previousScene == "cafe_white_ver")
+            {
+                player.transform.position = new Vector3(3.6f, -4, 0);
+            }
+            else if (previousScene == "Christmas")
+            {
+                player.transform.position = new Vector3(14.5f, -15, 0);
+            }
+            else if (previousScene == "Garden")
+            {
+                player.transform.position = new Vector3(13, 4, 0);
+            }
+
             GameObject mainCamera = Camera.main.gameObject;
             mainCamera.transform.parent = player.transform;
-            mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+            mainCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
         }
+        PlayerPrefs.SetString("PreviousScene", SceneManager.GetActiveScene().name);
     }
 }
