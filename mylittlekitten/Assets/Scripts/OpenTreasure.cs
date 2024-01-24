729e6a490521e78704b48c97aea997145ab93374
Jiwon, Cha <81519167+wona825@users.noreply.github.com>
2024-01-25 05:22:02 +0900

    feat/mainmap 보물 획득 시 화면에서 해당 보물 삭제 구현

--- mylittlekitten/Assets/Scripts/OpenTreasure.cs
+++ mylittlekitten/Assets/Scripts/OpenTreasure.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 public class OpenTreasure : MonoBehaviour
 {
     private Vector3 vector;
+
+    //������Ʈ ���� ����
     private CapsuleCollider2D capsuleCollider;
     public LayerMask layerMask;
     public float raycastLength;
     public bool openable;
+    public bool getable;
     public GameObject hitObject;
-    public GameObject openingMotion;
-    // AudioManager audioManager;
-
+    public GameObject OpenBoxMotion;
+    AudioManager audioManager;
+    public float horizontalsave;
+    public float verticalsave;
     void Start()
     {
         capsuleCollider = GetComponent<CapsuleCollider2D>();
-        // audioManager = FindObjectOfType<AudioManager>();
+        audioManager = FindObjectOfType<AudioManager>();
     }
 
     private void Update()
@@ -27,7 +31,10 @@ public class OpenTreasure : MonoBehaviour
 
         if (horizontalInput != 0 || verticalInput != 0)
         {
+            horizontalsave = horizontalInput;
+            verticalsave = verticalInput;
             openable = false;
+            getable = false;
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
 
             RaycastHit2D Interactivehit;
@@ -44,21 +51,31 @@ public class OpenTreasure : MonoBehaviour
             if (Interactivehit.transform != null)
             {
                 hitObject = Interactivehit.collider.gameObject;
-                print(hitObject.name);
-                openable = true;
+                if(hitObject.name=="carrot"){
+                    getable = true;
+                }else{
+                    openable=true;
+                }
             }
             else
             {
                 hitObject = null;
+                getable = false;
+                openable=false;
             }
         }
 
         if (openable && Input.GetKeyDown(KeyCode.Z))
         {
-            // audioManager.PlaySound("Opening");
+            //audioManager.PlaySound("Opening");
             Destroy(hitObject);
             StartCoroutine(OpeningMotion());
-            openable = false;
+        }
+
+        if (getable && Input.GetKeyDown(KeyCode.G))
+        {
+            //audioManager.PlaySound("Watering");
+            Destroy(hitObject);
         }
     }
 
@@ -66,13 +83,40 @@ public class OpenTreasure : MonoBehaviour
     {
         Vector3 openingVector = new Vector3(hitObject.transform.position.x, hitObject.transform.position.y, hitObject.transform.position.z);
 
-        GameObject newObject = Instantiate(openingMotion, openingVector, hitObject.transform.rotation);
+        GameObject newObject = Instantiate(OpenBoxMotion, openingVector, hitObject.transform.rotation);
 
         if (newObject.GetComponent<SpriteRenderer>() != null)
         {
-            newObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            newObject.GetComponent<SpriteRenderer>().sortingOrder = -3;
         }
+        openable=false;
+        raycasting();
+        yield break;
+    }
+    void raycasting(){
+            RaycastHit2D Interactivehit;
 
-        yield return new WaitForSeconds(2.0f); 
+            Vector2 inputDirection = new Vector2(horizontalsave, verticalsave).normalized;
+            Vector2 start = capsuleCollider.bounds.center;
+            Vector2 end = start + inputDirection * raycastLength;
+
+            capsuleCollider.enabled = false;
+            Interactivehit = Physics2D.Linecast(start, end, layerMask);
+            Debug.DrawRay(start, inputDirection * raycastLength, Color.green);
+            capsuleCollider.enabled = true;
+
+            
+            if (Interactivehit.transform != null)
+            {
+                hitObject = Interactivehit.collider.gameObject;
+                if(hitObject.name=="carrot"){
+                    getable = true;
+                }
+            }
+            else
+            {
+                hitObject = null;
+                getable = false;
+            }
     }
 }
