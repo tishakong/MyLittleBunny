706f1a58df8fe77d369606eec4f3cffad146a5b7
tishakong <leehn45@naver.com>
2024-01-25 05:22:34 +0900

    UIButtonClick소리 추가, 정원에 물주면 코인 증가 추가
    UIButtonClick소리 추가, 정원에 물주면 코인 증가 추가

--- mylittlekitten/Assets/Scripts/ChangeScene.cs
+++ mylittlekitten/Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+
+    AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
     public void ChangeScenefunc(){
         switch (this.gameObject.name)
         {
@@ -16,6 +24,8 @@ public class ChangeScene : MonoBehaviour
                 SceneManager.LoadScene("MainMap");
                 break;
         }
+
+        audioManager.PlaySound("UIButtonClick");
 
     }
     
